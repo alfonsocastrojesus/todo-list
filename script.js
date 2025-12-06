827afccd6b390ed174f25eecf409fbfe1281aa8d
castro_alfonso <alfonso.je.castro@gmail.com>
2025-12-05 20:54:57 -0600

    initial logic

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const todoInput = document.getElementById("todo-input");
-  const addTaskButton = document.getElementById("add-task-btn");
-  const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById("todo-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
 
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.forEach((task) => renderTask(task));
+let tasks = [];
 
-  addTaskButton.addEventListener("click", () => {
-    const taskText = todoInput.value.trim();
-    if (taskText === " ") return;
+addTaskBtn.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+  if (!taskText) return;
 
-    const newTask = {
-      id: Date.now(),
-      text: taskText,
-      completed: false,
-    };
-    tasks.push(newTask);
-    saveTasks();
-    console.log(tasks);
-  });
-
-  function renderTask(task) {
-    console.log(task);
-  }
-
-  function saveTasks(params) {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+  tasks.push(newTask);
+  todoInput.value = "";
+  console.log(tasks);
 });
+// the above code , gets the needed elements, creates a variable with an empty array, creates a function to check for user input, if text, then creates an object and save it to the tasks array (push) then clears the input field and console log the object in console , if the input field is empty , then just return nothing , the function stops. trim method clears the spaces after text in user input, date now creates a datestamp an unique number.
